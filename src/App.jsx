73070ffa54074b2b7f74ee21a8d95b727f838df8
gabzaok <gaabmoralles@gmail.com>
2025-07-26 10:08:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Header } from "./Components/Header";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="header">
+     <img className="size-4" src="/logo.svg" alt="" />
+    
+    </header>
+  );
+}
