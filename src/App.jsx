71548d413148ab79de549104adff0dfa66275ca3
gabzaok <gaabmoralles@gmail.com>
2025-09-16 20:28:14 -0300

    feat: pass props for components

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { Home } from "./Pages/home";
 import { Form } from "./Components/Form";
 
 export const App = () => {
-  const [isShowForm, setIsShowForm] = useState(true);
+  const [isShowForm, setIsShowForm] = useState(false);
   return (
     <div
       style={{ backgroundImage: "url('/background.jpg')" }}
       className="h-screen bg-cover bg-center"
     >
-      {isShowForm ? <Form /> : <Home />}
+      {isShowForm ? (
+        <Form setIsShowForm={setIsShowForm} />
+      ) : (
+        <Home setIsShowForm={setIsShowForm} />
+      )}
     </div>
   );
 };
