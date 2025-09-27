b7d5a32d890996984eb51e4228668c355e994801
gabzaok <gaabmoralles@gmail.com>
2025-09-27 19:27:44 -0300

    dev: defaultValue added for testing

--- src/constants/inputs.js
+++ src/constants/inputs.js
@@ -1,5 +1,6 @@
 export const INPUTS = [
   {
+    defaultValue: "São Luís - MA",
     icon: "/add location.svg",
     placeholder: "Ex: São Luís - MA",
     type: "text",
@@ -8,12 +9,14 @@ export const INPUTS = [
   },
   {
     icon: "/add actv.svg",
+    defaultValue: "Visitar o Centro Histórico",
     placeholder: "Ex: Visitar o Centro Histórico",
     type: "text",
     label: "Adicionar Atividade",
     id: "inputActivity",
   },
   {
+    defaultValue: new Date().toISOString().split("T")[0],
     icon: "/add date.svg",
 
     type: "date",
@@ -22,7 +25,7 @@ export const INPUTS = [
   },
   {
     icon: "/add time.svg",
-
+    defaultValue: "12:00",
     type: "time",
     label: "Adicionar Hora",
     id: "inputHour",
