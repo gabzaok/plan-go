fe59fe4fe2f27c4ca6e49f114d8caeb91e15d6a3
gabzaok <gaabmoralles@gmail.com>
2025-09-27 14:12:04 -0300

    feat: add input constants

---
+++ src/constants/inputs.js
@@ -0,0 +1,30 @@
+export const INPUTS = [
+  {
+    icon: "/add location.svg",
+    placeholder: "Ex: São Luís - MA",
+    type: "text",
+    label: "Adicionar Destino",
+    id: "inputDestination",
+  },
+  {
+    icon: "/add actv.svg",
+    placeholder: "Ex: Visitar o Centro Histórico",
+    type: "text",
+    label: "Adicionar Atividade",
+    id: "inputActivity",
+  },
+  {
+    icon: "/add date.svg",
+
+    type: "date",
+    label: "Adicionar Data",
+    id: "inputDate",
+  },
+  {
+    icon: "/add time.svg",
+
+    type: "time",
+    label: "Adicionar Hora",
+    id: "inputHour",
+  },
+];
